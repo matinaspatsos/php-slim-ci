c28de33ac7754420425dec4d8d690a3cd1b52e78
matinaspatsos <matinaspatsos@gmail.com>
2024-02-15 18:33:12 -0500

    Remove SAST finding

--- src/Example.php
+++ src/Example.php
@@ -4,19 +4,8 @@ namespace App;
 
 class Example
 {
-  public function testCiCd(): string
-  {
-    return "What?";
-  }
   public static function test(): string
   {
     return "test";
-  }
-  public function sastFinding()
-  {
-    $password = "1234";
-    $offset = $_GET['offset']; // beware, no input validation!
-    $query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
-    $result = pg_query($conn, $query);
   }
 }
